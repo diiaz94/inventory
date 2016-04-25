c9dbddeafa4a08dd271180d156a8ddd8d7754aef
unknown <VP50343@BPVMXL537202N.bpv.provincial.corp>
2016-04-25 18:55:28 -0430

    mensaje

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,10 @@ class WelcomeController < ApplicationController
   def index
   	if User.all.count==0
   		render "register_admin" , layout: "blank"
+  	else
+  		if !current_user
+  			redirect_to(login_path,alert: "Lo sentimos, debe iniciar sesión primero")
+  		end
   	end
   end
 end
